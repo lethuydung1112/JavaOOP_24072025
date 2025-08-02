969ab83a5cc2daaf278bf90eda7dc5736b4f0079
lethuydung1112 <leethuydung789@gmail.com>
2025-08-02 22:23:55 +0700

    Update Information.java

--- src/PhamViTruyCap_BT2/person/Information.java
+++ src/PhamViTruyCap_BT2/person/Information.java
@@ -1,4 +1,13 @@
 package PhamViTruyCap_BT2.person;
 
 public class Information {
+    public static void main(String[] args) {
+        Person ps1 = new Person("dung", 22, "female", "HN", "01234567");
+
+        ps1.getName();
+        ps1.getAge();
+        ps1.getAddress();
+        ps1.getGender();
+        ps1.getPhone();
+    }
 }
